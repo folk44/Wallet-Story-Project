a704e5940f34b20430c66a8586905c870af137a7
ForestGoose <chayawat.pahn1910@gmail.com>
2020-10-19 00:05:14 +0700

    Adding Menu.

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,12 +1,29 @@
 #include <graphics.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main( )
 {
-	initwindow(400, 300, "First Sample");
-    circle(100, 50, 40);
-    while (!kbhit( ))
-    {
-        delay(200);
-    }
-    return 0;
+	int menu, balance = 0;
+	do{
+		system("cls");
+		printf("Balance : %d\n",balance);
+		printf("1: \n");
+		printf("2: \n");
+		printf("3: \n");
+		printf("0: Exit\n");
+		do{
+			printf("\nEnter your choice: ");
+			scanf("%d",&menu);
+			if(menu < 0 || menu > 3) printf("Invalid choice. Please try again.\n");
+		}while(menu < 0 || menu > 3);
+		switch(menu){
+			case 1: printf("Go to menu 1.\n"); break;
+			case 2: printf("Go to menu 2.\n"); break;
+			case 3: printf("Go to menu 3.\n"); break;
+			default: printf("Thank you.");
+		}
+		delay(300);
+	}while(menu != 0);
+	return 0;
 }
