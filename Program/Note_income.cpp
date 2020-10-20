17bcecb43dda7e81760d2398e4a96b5dc82e8977
ForestGoose <chayawat.pahn1910@gmail.com>
2020-10-20 14:11:05 +0700

    Edit to delay().

--- Program/Note_income.cpp
+++ Program/Note_income.cpp
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<windows.h>
-#include"income.h"
+#include <graphics.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "income.h"
 void Hello(){
 	system("cls");
 	printf("Hello world\n");
-	Sleep(1000);
+	delay(1000);
 }
