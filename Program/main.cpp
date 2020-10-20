80b193989ae1a6977e2b3127aec2442aa16b84ed
ForestGoose <chayawat.pahn1910@gmail.com>
2020-10-20 09:38:46 +0700

    Adding description.

--- Program/main.cpp
+++ Program/main.cpp
@@ -7,16 +7,23 @@ int main( )
 	int menu, balance = 0;
 	do{
 		system("cls");
+		//Show Balanve
 		printf("Balance : %d\n",balance);
+		
+		//Display Menu
 		printf("1: \n");
 		printf("2: \n");
 		printf("3: \n");
 		printf("0: Exit\n");
+		
+		//Check valid menu. If invalid, inform to user.		
 		do{
 			printf("\nEnter your choice: ");
 			scanf("%d",&menu);
 			if(menu < 0 || menu > 3) printf("Invalid choice. Please try again.\n");
 		}while(menu < 0 || menu > 3);
+		
+		//Go to selected menu.
 		switch(menu){
 			case 1: printf("Go to menu 1.\n"); break;
 			case 2: printf("Go to menu 2.\n"); break;
